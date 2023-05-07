9a0c537907bd6afb700bf3c9f0ff8e93da53cbd9
BlackHaroto <florene94240@gmail.com>
2023-05-07 12:34:39 +0200

    NEW VERSION ! Mise en place de la page d'accueil avec les boutons pour choisir son mode Coco tu te charges de la mise en beauté ? 👈(ﾟヮﾟ👈)

--- app/src/main/java/com/acs/readertest/MainActivity.java
+++ app/src/main/java/com/acs/readertest/MainActivity.java
@@ -110,8 +110,8 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
-            case R.id.action_developpers_mode:
-                //dev_mode(); <-- Léo je te laisse faire le code brut et je m'occupe de la mise en beauté;)
+            case R.id.action_dev_mode:
+                //dev_mode(); <-- A FAIRE !!!
                 Toast.makeText(getApplicationContext(),"Vous avez cliqué sur mode développeur", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.action_settings:
