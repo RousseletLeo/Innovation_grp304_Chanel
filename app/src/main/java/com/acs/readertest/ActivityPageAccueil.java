5b48cf4b1dd448c99b3cb6367bcaf5baf77b7bba
BlackHaroto <florene94240@gmail.com>
2023-05-08 17:40:50 +0200

    Là, il y a la page d'accueil avec un bouton "mode simplifié" qui renvoie à ActivitySimpleMode et un autre bouton "mode développeur" qui renvoie à MainActivity !! Et quand on clique dessus, bah ça fonctionne XD

---
+++ app/src/main/java/com/acs/readertest/ActivityPageAccueil.java
@@ -0,0 +1,30 @@
+package com.acs.readertest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class ActivityPageAccueil extends Activity {
+    private static final String LOG_TAG =ActivityPageAccueil.class.getSimpleName();
+    private static final String LOG_TAG_2 =ActivityPageAccueil.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_accueil);
+
+    }
+
+    public void launchMainActivity(View view) {
+        Log.d(LOG_TAG, "Boutton cliqué !");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchActivitySimpleMode(View view) {
+        Log.d(LOG_TAG_2, "Boutton cliqué !");
+        Intent intent2 = new Intent(this, ActivitySimpleMode.class);
+        startActivity(intent2);
+    }
+}
