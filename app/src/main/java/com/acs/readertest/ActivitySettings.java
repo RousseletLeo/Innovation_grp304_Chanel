111e3f02aa185163c983b0dc9b3f3c75cc37e77f
BlackHaroto <florene94240@gmail.com>
2023-05-14 19:50:31 +0200

    J'essaye de sauvegarder les spinner lorsqu'on clique sur 'sauvegarder' mais ça fonctionne pas des masses quand on change de page...

--- app/src/main/java/com/acs/readertest/ActivitySettings.java
+++ app/src/main/java/com/acs/readertest/ActivitySettings.java
@@ -2,18 +2,28 @@ package com.acs.readertest;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.widget.Spinner;
-import android.widget.Toast;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivitySettings extends Activity {
+    TextView messageView;
+    Context context;
+    Resources resources;
+    private static final String SELECTED_ITEM = "SELECTED_ITEM";
+    private Spinner mSpinner;
+    private int mSelectedItem = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +35,44 @@ public class ActivitySettings extends Activity {
         rotateAnimation.setDuration(1000);
         rotateAnimation.setRepeatCount(Animation.INFINITE);
         ic_settings.startAnimation(rotateAnimation);
+
+
+        //
+        mSpinner = findViewById(R.id.sp_langue);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_langues, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(adapter);
+
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM);
+            mSpinner.setSelection(mSelectedItem);
+        }
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mSelectedItem = position;
+                // Do something with the selected item
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SELECTED_ITEM, mSelectedItem);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM);
     }
 
     //Mettre en pause l'animation lorsqu'on quitte la page
@@ -33,24 +81,6 @@ public class ActivitySettings extends Activity {
         super.onPause();
         ImageView gearImageView = findViewById(R.id.imageView2);
         gearImageView.clearAnimation();
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                Intent intent2 = new Intent(this, ActivitySettings.class);
-                startActivity(intent2);
-                return true;
-            case R.id.action_help:
-                Intent intent3 = new Intent(this, ActivityHelp.class);
-                startActivity(intent3);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 
     public Integer maj_autorisations(View view) {
@@ -75,18 +105,26 @@ public class ActivitySettings extends Activity {
     public void sauvegarder(View view) {
         Spinner spinner = findViewById(R.id.sp_langue);
         String langue = spinner.getSelectedItem().toString();
-        SharedPreferences prefs = getSharedPreferences("base_de_donnees", Context.MODE_PRIVATE);
+
+        Log.d("TAG", langue);
+
+        SharedPreferences prefs = getSharedPreferences("strings", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString("language", langue);
         editor.apply();
 
         Spinner spinner_2 = findViewById(R.id.sp_theme);
         String theme = spinner_2.getSelectedItem().toString();
-        SharedPreferences prefs_2 = getSharedPreferences("base_de_donnees", Context.MODE_PRIVATE);
+        SharedPreferences prefs_2 = getSharedPreferences("strings", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor_2 = prefs_2.edit();
         editor_2.putString("theme_spinner", theme);
         editor_2.apply();
 
+        context = LocaleHelper.setLocale(ActivitySettings.this, "en");
+        resources = context.getResources();
+
         Toast.makeText(this, "Changements sauvegardés !", Toast.LENGTH_SHORT).show();
+
+        recreate();
     }
 }
